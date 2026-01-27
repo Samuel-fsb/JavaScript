73fa8b5adf816c5c5891f87173027dd66c9a6eab
Samuel <samuel.fsbs@gmail.com>
2026-01-27 15:08:03 -0300

    Estudando o uso de botões.

---
+++ exercicios/exd007/scripts/script.js
@@ -0,0 +1,13 @@
+function pergunta(){
+    let nome = window.prompt('Qual é o seu nome?')
+    window.alert(`É um prazer em lhe conhecer ${nome}`)
+    window.document.body.style.button.backgroundColor = 'blue'
+}
+
+
+
+/*
+const clicou = 'você clicou no botão' =>{
+
+}
+    */
