d32a6ea79a2a42bdf886c0d7efd017917e51ebd2
Samuel <samuel.fsbs@gmail.com>
2026-01-31 08:45:24 -0300

    Usando o operador de atribuição em botões.

---
+++ exercicios/aquivo.js
@@ -0,0 +1,17 @@
+let res = window.document.getElementById('saida');
+
+function botao1(){
+    res.innerHTML = '<p> Você clicou no botão 1 </p>'
+};
+
+function botao2(){
+    res.innerHTML = ' <p> Você clicou no botão 2 </p>'
+};
+
+function botao3(){
+    res.innerHTML += ' <p> Você clicou no botão 3 </p> '
+};
+
+function botao4(){
+    res.innerHTML += ' <p> Você clicou no botão 4 </p> '
+};
