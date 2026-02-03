29f1b139765bba1645bbcae8543fe16adce3f6d4
Samuel <samuel.fsbs@gmail.com>
2026-02-03 14:25:11 -0300

    Criando um conversou de temperaturas.

--- exercicios/aquivo.js
+++ exercicios/aquivo.js
@@ -16,7 +16,7 @@ function botao4(){
     res.innerHTML += ' <p> Você clicou no botão 4 </p> '
 };
 
-let converter = window.document.getElementById('con')
+let converter = window.document.getElementById('con');
 
 function conversor(){
     let distancia = Number(window.prompt('Digite uma distância em metros (m)'))
@@ -28,8 +28,8 @@ function conversor(){
     let mm = distancia * 1000
 
     converter.innerHTML += `<h2> A distância de ${distancia} metros, corresponte a...</h2> <P> ${km.toLocaleString('pt-BR')} Quilômetros (Km) <br> ${hm.toLocaleString('pt-BR')} Hectômetro (Hm) <br> ${dam.toLocaleString('pt-BR')} Decâmetros (Dam) <br> ${dm.toLocaleString('pt-BR')} Decímtros (dm) <br> ${cm.toLocaleString('pt-BR')} Centrímetros (cm) <br> ${mm.toLocaleString('pt-BR')} Milímetros (mm)</p>                                                    `
-}
+};
 
 function limpar(){
     converter.innerHTML = ('Aqui vai aparecer os resultados.')
-}
+};
