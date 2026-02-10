7ce64dca6232cac2b528fe4d2bca389cf63cc38e
Samuel <samuel.fsbs@gmail.com>
2026-02-10 14:54:56 -0300

    Resolvendo exercício com forEach.

--- exercicios/desafios/arquivo3.js
+++ exercicios/desafios/arquivo3.js
@@ -15,3 +15,40 @@ const SomaProdutos = produtos.filter(produto => produto.preco >= 100)
                                 .reduce((acumulador, atual) => {return acumulador + atual.preco}, 0)
 
 console.log(`A soma de todos os protudos maiores ou iguais a cem reais é de: ${SomaProdutos}`);
+
+
+
+const produtosMaisCem = produtos.filter(produto => produto.preco >= 100)
+                                .reduce((acumulador, atual) => {return acumulador + atual.preco}, 0)
+                                .map(produto => `O ${produto.nome} custa ${produto.preco}`)
+
+console.log(produtosMaisCem);
+
+const SomaProdutos = produtos.filter(produto => produto.preco >= 100)
+.reduce((acumulador, atual) => {return acumulador + atual.preco}, 0)
+
+console.log(`A soma de todos os produtos maiores ou iguais a 100 reais é de: ${SomaProdutos}`);
+
+
+const GerarRelatório = listaDeProdutos => {
+
+    const filtrados = listaDeProdutos.filter(f => f.preco >= 100)
+    
+    const frases = filtrados.map(`produto => O ${produto.nome} custa ${produto.preco}`)
+
+};
+
+console.log(filtrados);
+
+
+const convidados = ["Carlos", "vip", "Mariana", "vip", "José"];
+
+const convidadosVIP = convidados.forEach(nome => {
+    if (nome === "vip"){
+        console.log(nome.toUpperCase());
+    } else {
+        console.log(nome);
+    };
+});
+
+console.log(convidadosVIP);
