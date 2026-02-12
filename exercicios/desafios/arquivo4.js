9314e381e91661eb9597a91066f3fee05b623652
Samuel <samuel.fsbs@gmail.com>
2026-02-12 09:03:38 -0300

    Aprendendo sobre chaves de objetos.

--- exercicios/desafios/arquivo4.js
+++ exercicios/desafios/arquivo4.js
@@ -1,8 +1,18 @@
 const DadosPessoais = {
     nome: "José",
     numero: "66 6666 66666",
-    "rua": "Carlinhos"
+    rua: "Carlinhos"
 };
 
-console.log(DadosPessoais["rua"]);
+/*let campoDesejado = "emali";*/
+console.log(DadosPessoais.rua);
+
+
+let campoDesejado = "email";
+let usuario = { nome: "Carlos", email: "carlos@exemplo.com" };
+
+usuario[campoDesejado] = "novo@email.com";
+
+console.log(usuario.email);
+
 
