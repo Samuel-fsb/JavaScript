ae7013e52115fb4c6b8037d6b0d7669742d96b5a
Samuel <samuel.fsbs@gmail.com>
2026-02-12 12:11:16 -0300

    Criando função e interagindo com a página usando o DOM.

--- exercicios/desafios/arquivo4.js
+++ exercicios/desafios/arquivo4.js
@@ -4,8 +4,8 @@ const DadosPessoais = {
     rua: "Carlinhos"
 };
 
-/*let campoDesejado = "emali";*/
-/*console.log(DadosPessoais.rua);
+let campoDesejado = "emali";
+console.log(DadosPessoais.rua);
 
 
 let campoDesejado = "email";
@@ -26,7 +26,7 @@ const estudante = { nome: "João", idade: 22, curso: "ADS"};
 
 const ChavesDoEstudante = (Object.entries(estudante));
 
-console.log(ChavesDoEstudante);*/
+console.log(ChavesDoEstudante);
 
 const precos =  {maca: 5.50, banana: 3.00, uva: 8.20};
 
@@ -35,6 +35,34 @@ const SoPrecos = Object.values(precos);
 const SomaPrecos = SoPrecos.reduce((acumulador, atual) => acumulador + atual, 0);
 
 console.log(SomaPrecos);
+
+const usuarios = [
+    { id: 1, nome: "Ana"},
+    { id: 2, nome: "Beto"},
+    { id: 3, nome: "Beto"}
+];
+
+const NovoUsuario = usuarios.find(u => u.nome == "Ana");
+
+console.log(NovoUsuario);
+
+
+let fila = ["documento_pdf", "fotos_ferias", "trabalho_escola"];
+
+fila.push("relatório_final");
+
+const ProcurarFoto = fila.find(f => f === "fotos_ferias");
+
+const ArquivoRemovido = fila.pop()
+
+console.log(fila);
+console.log(ProcurarFoto);
+console.log(`Arquivo removido [${ArquivoRemovido}]`);
+
+
+
+
+
 
 
 
