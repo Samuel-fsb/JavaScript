ba583c49e37c29eb39dcfacda1ce53ba28d9ed0e
Samuel <samuel.fsbs@gmail.com>
2026-04-16 14:33:53 -0300

    Estudando estruturas condicionais.

--- exercicios/exd011/aquivo.js
+++ exercicios/exd011/aquivo.js
@@ -5,3 +5,28 @@ function getInitials(name, lastName){
 
 getInitials('Alice', 'Green');
 getInitials('Bob', 'Smith');
+
+function printOddOrEven(n){
+  if(n % 2 === 0){
+    console.log("even");
+  } else if(n % 2 !== 0){
+    console.log("odd");
+  };
+};
+
+function checkWordLength(word) {
+  if(word.length < 5){
+    console.log(word + " is a short word");
+  } else if(word.length >= 5){
+    console.log(word + " is a long word");
+  };
+};
+
+function canBuyBeer(age){
+  if(age < 18){
+    return "You cannot buy beer";
+  }
+  if(age >= 18){
+    return "You can buy beer";
+  };
+};
