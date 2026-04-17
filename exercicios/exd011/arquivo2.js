5ac7dbdc48c37e83a17df9e196baf3e92fd5c77a
Samuel <samuel.fsbs@gmail.com>
2026-04-17 18:42:50 -0300

    Trabalhando com for e if.

--- exercicios/exd011/arquivo2.js
+++ exercicios/exd011/arquivo2.js
@@ -13,3 +13,20 @@ function restoreSpaces(input) {
 function isSubstring(phrase, part) {
  return phrase.includes(part);
 };
+
+function getPosition(text, word) {
+  return text.indexOf(word);
+};
+
+function countConsonants(input) {
+  let count = 0;
+  const consoantes = 'bcdfghjklmnpqrstvwxz';
+
+for(let i = 0 ; i < input.length ; i++){
+  if(consoantes.includes(input[i].toLocaleLowerCase())){
+    count += 1;
+  };
+};
+  
+  return count;
+}; 
