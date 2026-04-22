a895ebca63c16c616d8669017d72b91e91f0939f
Samuel <samuel.fsbs@gmail.com>
2026-04-22 16:33:14 -0300

    Acessando valores de arrays e retornando novos arrays.

--- exercicios/exd011/arquivo3.js
+++ exercicios/exd011/arquivo3.js
@@ -96,3 +96,89 @@ getCentury(2001)
 getCentury(0)
 getCentury(1786) 
 getCentury(1500)
+
+function makeDecision(fuelRemaining, distance, fuelConsumption) {
+
+   if(distance < 0 || fuelRemaining < 0 || fuelConsumption <= 0) {
+    return 'please, enter the valid data';
+   }
+
+  let distanciaPossivel = (fuelRemaining / fuelConsumption) * 100;
+
+   
+   if(distanciaPossivel >= distance) {
+    return 'reach gas station by themselves';
+  } else if(distanciaPossivel < distance) {
+    return 'ask for help';
+  }
+}
+
+makeDecision(10, 34, 20) 
+makeDecision(0, 34, 20) 
+makeDecision(-2, -30, 0) 
+makeDecision(2, 25, 8)
+
+function convertCurrency(amount, exchangeRate, currencyName) {
+  
+  if(amount <= 0 || exchangeRate <= 0){
+    return 'Enter valid data';
+  }
+  
+  let conversao = amount * exchangeRate;
+  let formatado = +conversao.toFixed(2);
+
+  if(amount > 0 && exchangeRate > 0){
+    return `Give them ${formatado} ${currencyName}(s)`;
+  }
+}
+
+convertCurrency(10000, 1.0946, 'dollar');
+convertCurrency(250, 4.338, 'zloty'); 
+convertCurrency(1, 40.7131, 'hryvnia');
+convertCurrency(1, 5.3364, 'Real'); 
+convertCurrency(-100, -0.034, 'bolivar');
+
+function getPlan(startProduction, numberOfMonths, percent) {
+  let goals = [];
+
+  let currentProduction = startProduction;
+
+  for(let i = 0 ; i < numberOfMonths ; i++) {
+    currentProduction = Math.floor(currentProduction + (currentProduction * (percent / 100)))
+    goals.push(currentProduction)
+  }
+  return goals;
+}
+
+getPlan(10, 4, 30); 
+getPlan(1000, 6, 20); 
+
+function getSpeedStatistic(testResults) {
+  let velocity = [];
+  let max = testResults[0];
+  let min = testResults[0];
+  let sum = 0;
+  let average = 0;
+
+  if(testResults.length === 0){
+    return [0, 0, 0];
+  }
+
+  for(let i = 0 ; i < testResults.length ; i++){
+    if(testResults[i] > max){
+      max = testResults[i];
+    }
+    if(testResults[i] < min){
+      min = testResults[i];
+    }
+    sum += testResults[i];
+    average = Math.floor(sum / testResults.length);
+    velocity.push(average);
+  }
+  return [min, max, average];
+}
+
+getSpeedStatistic([]); 
+getSpeedStatistic([10]); 
+getSpeedStatistic([8, 9, 3, 12]);
+getSpeedStatistic([10, 10, 11, 9, 12, 8]); 
