043cfdfee9f5fcdd6b32bd6ff586455bebd78045
Samuel <samuel.fsbs@gmail.com>
2026-06-05 10:11:08 -0300

    Criando classes e gerando a herança para outras classes.

---
+++ exercicios/exd011/arquivo6.js
@@ -0,0 +1,68 @@
+class BaseRobot {
+  constructor(name, weight, coords, chipVersion) {
+    this.name = name;
+    this.weight = weight;
+    this.chipVersion = chipVersion;
+    this.coords = {
+      x: coords.x || 0,
+      y: coords.y || 0,
+    };
+  };
+
+  getInfo() {
+    return `Robot: ${this.name}, Chip version: ${this.chipVersion}, Weight: ${this.weight}`;
+  }
+
+  goForward(step = 1) {
+    this.coords.y += step;
+  };
+
+  goBack(step = 1) {
+    this.coords.y -= step;
+  };
+
+  goRight(step = 1) {
+    this.coords.x += step;
+  };
+
+  goLeft(step = 1) {
+    this.coords.x -= step;
+  };
+}
+
+class FlyingRobot extends BaseRobot{
+  constructor(name, weight, coords, chipVersion) {
+    super(name, weight, coords, chipVersion);
+
+    this.coords.z = coords.z || 0;
+  };
+
+  goUp(step = 1) {
+    this.coords.z += step;
+  };
+
+  goDown(step = 1) {
+    this.coords.z -= step;
+  };
+}
+
+class DeliveryDrone extends FlyingRobot{
+  constructor(name, weight, coords, chipVersion, maxLoadWeight = null, currentLoad = null) {
+    super(name, weight, coords, chipVersion);
+
+    this.maxLoadWeight = maxLoadWeight;
+    this.currentLoad = currentLoad;
+  }
+
+  hookLoad(cargo) {
+    if (this.currentLoad || cargo.weight > this.maxLoadWeight) {
+      return;
+    }
+
+    this.currentLoad = cargo;
+  };
+
+  unhookLoad() {
+    this.currentLoad = null;
+  };
+}
