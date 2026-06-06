856408abf0ca959ac7f996f6f2c4e83d3b90caee
Samuel <samuel.fsbs@gmail.com>
2026-06-06 09:36:25 -0300

    Percorrendo um array e retornando o século que a pessoa viveu.

--- exercicios/exd011/arquivo6.js
+++ exercicios/exd011/arquivo6.js
@@ -66,3 +66,55 @@ class DeliveryDrone extends FlyingRobot{
     this.currentLoad = null;
   };
 }
+
+function getPeopleWithCentury(people) {
+  return people.map(person => ({
+    ...person, century: Math.ceil((person.died / 100)),
+  }),
+  );
+}
+
+const people = [
+  { name: 'Carolus Haverbeke', born: 1832, died: 1905 },
+  { name: 'Emma de Milliano', born: 1876, died: 1956 },
+  { name: 'Maria de Rycke', born: 1683, died: 1724 },
+  { name: 'Carel Haverbeke', born: 1796, died: 1837 },
+];
+
+getPeopleWithCentury(people) === [
+  { name: 'Carolus Haverbeke', born: 1832, died: 1905, century: 20 },
+  { name: 'Emma de Milliano', born: 1876, died: 1956, century: 20 },
+  { name: 'Maria de Rycke', born: 1683, died: 1724, century: 18 },
+  { name: 'Carel Haverbeke', born: 1796, died: 1837, century: 19 },
+];
+
+function getPeopleWithChildren(people) {
+  return people.map(person => ({
+    ...person,
+    children: people.filter(child =>
+      child.father === person.name || child.mother === person.name),
+  }));
+}
+
+const people = [
+  { name: 'Carolus Haverbeke', father: 'Carel Haverbeke', mother: 'Maria van Brussel' },
+  { name: 'Emma de Milliano', father: 'Petrus de Milliano', mother: 'Sophia van Damme' },
+  { name: 'Laurentia Haverbeke', father: 'Jan Haverbeke', mother: 'Maria de Rycke' },
+  { name: 'Maria de Rycke', father: 'Frederik de Rycke', mother: 'Laurentia van Vlaenderen' },
+  { name: 'Carel Haverbeke', father: 'Pieter Antone Haverbeke', mother: 'Livina Sierens' },
+  { name: 'Elisabeth Haverbeke', father: 'Jan Haverbeke', mother: 'Maria de Rycke' },
+];
+
+const [carolus, emma, laurentina, maria, carel, elisabeth] = getPeopleWithChildren(people);
+
+console.log(
+  carolus.name,
+  carolus.children.length, 
+);
+
+console.log(
+  maria.name,
+  maria.children.length,
+  maria.children[0] === laurentina, 
+  maria.children[1] === elisabeth, 
+);
