5e888975164df58021c054cb4a0062cf7512ca58
Samuel <samuel.fsbs@gmail.com>
2026-06-10 09:58:54 -0300

    Abaixando o compilador do TypeScript.

---
+++ exercicios/exd012/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function calculate(x, y, z) {
+    const result = (x + y) * z;
+    console.log(`(${x} + ${y}) * ${z} = ${result}`);
+}
+calculate(2, 3, 10);
+calculate(1, 3, 5);
+//# sourceMappingURL=app.js.map
---
+++ exercicios/exd012/app.ts
@@ -0,0 +1,8 @@
+function calculate(x: number, y:number, z: number) {
+    const result = (x + y) * z;
+
+    console.log(`(${x} + ${y}) * ${z} = ${result}`);
+}
+
+calculate(2, 3, 10);
+calculate(1, 3, 5);
