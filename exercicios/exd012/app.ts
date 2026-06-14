c5e1cc6ab1be7f9e3177d430af3bc4e3ef61c3a2
Samuel <samuel.fsbs@gmail.com>
2026-06-14 11:22:48 -0300

    Tipando funções e criando um objeto com tipos.

--- exercicios/exd012/app.ts
+++ exercicios/exd012/app.ts
@@ -6,3 +6,15 @@ function calculate(x: number, y:number, z: number) {
 
 calculate(2, 3, 10);
 calculate(1, 3, 5);
+
+'use strict';
+
+type UserId = string | number;
+function getBinaryId(userId: UserId): string {
+  if (typeof userId === 'number') {
+    return userId.toString(2);
+  }
+
+  return parseInt(userId, 16).toString(2);
+  
+}
